34a825fcdd500884528bb843fe36d0224f3b09a3
jgaar <jgaar@10.153.238.40>
2019-01-09 11:08:26 +0100

    JDBC class added

---
+++ Musiksystem/src/data/JDBC.java
@@ -0,0 +1,54 @@
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+
+	private Connection connection;
+	
+	public JDBC() {
+	
+	if (!loadJDBCDriver())
+		System.exit(1);
+
+	if (!openConnection("BravoMusicDB"))
+		System.exit(2);
+	
+	}
+	
+	private boolean loadJDBCDriver() {
+		System.out.println("Loading JDBC driver...");
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			System.out.println("JDBC driver loaded");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Failed to load JDBC driver");
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean openConnection(String databaseName) {
+
+		String connectionString = "jdbc:sqlserver://localhost:1433;" + "instanceName=SQLEXPRESS;" + "databaseName="
+				+ databaseName + ";" + "integratedSecurity=true;";
+
+		System.out.println("Connecting to database...");
+
+		try {
+			connection = DriverManager.getConnection(connectionString);
+			System.out.println("Database connection established");
+		} catch (SQLException e) {
+			System.out.println("Failed to connect to database");
+			System.out.println(e.getMessage());
+			return false;
+		}
+
+		return true;
+	}
+
+}
