7f753c7e7eb5c59e206ac504715dca83dc35098a
silke <silke@10.153.246.55>
2019-01-09 14:05:24 +0100

    dfhd

--- Musiksystem/src/data/SongDBCalls.java
+++ Musiksystem/src/data/SongDBCalls.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,6 +9,8 @@ import java.sql.Statement;
 import logic.domainClasses.Song;
 
 public class SongDBCalls {
+	
+	//private static Connection connection;
 	public static boolean updateSong(Song song) {
 		try {
 			PreparedStatement stmt = JDBC.connection.prepareStatement("UPDATE song "
@@ -50,30 +53,29 @@ public class SongDBCalls {
 		
 	public static boolean addSong(Song song) {
 		try {
-			PreparedStatement stmt = JDBC.connection.prepareStatement("INSERT INTO song VALUES " +
-						"albumId = " + song.getAlbumId() +
-						"artistId = " + song.getArtistId() + 
-						"conductorId = " + song.getConductorId() +
-						"songName = ?" +
-						", genre = '" + song.getGenre() + 
-						"' " /*, time = ?" */ + 
-						", songwriter = ?" + 
-						", songNote = ?");
-			
-			stmt.setString(1, song.getSongName());
-//			stmt.setTime(2, song.getTime());
-			stmt.setString(3, song.getSongwriter());
-			stmt.setString(4, song.getSongNote());
-			
+			JDBC.loadJDBCDriver();
+			JDBC.openConnection("BravoMusicDB");
+
+		    String query = "INSERT INTO song (albumId, artistId, conductorId, songName, genre, songwriter, songNote)" + " values (?, ?, ?, ?, ?, ?, ?)";
+
+		          // create the mysql insert preparedstatement
+		          PreparedStatement preparedStmt = JDBC.connection.prepareStatement(query);
+		          preparedStmt.setInt(1, song.getAlbumId());
+		          preparedStmt.setInt(2, song.getArtistId());
+		          preparedStmt.setInt(3, song.getConductorId());
+		          preparedStmt.setString(4, song.getSongName());
+		          preparedStmt.setObject(5, song.getGenre());
+		          preparedStmt.setString(6, song.getSongwriter());
+		          preparedStmt.setString(7, song.getSongNote());
 //			System.out.println(sql);
 			
-			int nRows = stmt.executeUpdate();
+			int nRows = preparedStmt.executeUpdate();
 			if (nRows != 1) {
 				return false;
 			}
 			
 			//get auto-generated key
-			ResultSet resultSet = stmt.executeQuery("SELECT SCOPE_IDENTITY()");
+			ResultSet resultSet = preparedStmt.executeQuery("SELECT SCOPE_IDENTITY()");
 			if (resultSet.next()) {
 				int id = resultSet.getInt(1);
 				song.setSongId(id);
@@ -82,6 +84,7 @@ public class SongDBCalls {
 		}
 		catch (SQLException e) {
 			System.out.println("Could not add song: " + song);
+			System.out.println(e.getMessage());
 			return false;
 		}
 	}
