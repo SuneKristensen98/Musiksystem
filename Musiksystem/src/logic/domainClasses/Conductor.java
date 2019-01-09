41f1a046aa949a2ef3204e749a0f3eeaff5e78f4
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-09 12:27:45 +0100

    Conductor.logic

--- Musiksystem/src/logic/domainClasses/Conductor.java
+++ Musiksystem/src/logic/domainClasses/Conductor.java
@@ -1,5 +1,34 @@
 package logic.domainClasses;
 
 public class Conductor {
+	
+
+
+		protected int conductorId;	
+		protected String conductorName;
+		
+
+		public Conductor(int conductorId, String conductorName) {
+			this.conductorId = conductorId;
+			this.conductorName = conductorName;
+
+
+		}
+
+
+		public int getConductorId() {
+			return conductorId;
+		}
+
+
+		public void setConductorId(int conductorId) {
+			this.conductorId = conductorId;
+		}
+
+
+		public String getConductorName() {
+			return conductorName;
+		}
+	
 
 }
