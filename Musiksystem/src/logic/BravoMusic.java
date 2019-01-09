4947fa83c6767f618020c5a519fb3ecb8dec6d19
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-09 14:26:13 +0100

    bravomusic

--- Musiksystem/src/logic/BravoMusic.java
+++ Musiksystem/src/logic/BravoMusic.java
@@ -1,5 +1,20 @@
 package logic;
 
+import logic.domainClasses.Album;
+import logic.domainClasses.Song;
+
 public interface BravoMusic {
+
+	public boolean addAlbum(Album album);
+
+	public boolean updateAlbum(Album album);
+
+	public boolean deleteAlbum(int albumId);
+
+	public boolean addSong(Song song);
+
+	public boolean updateSong(Song song);
+
+	public boolean deleteSong(int songId);
 
 }
