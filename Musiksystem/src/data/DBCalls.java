50e7233bf809c14ab49f317c9697b3d719d74781
silke <silke@10.153.246.55>
2019-01-09 11:44:15 +0100

    Method added

--- Musiksystem/src/data/DBCalls.java
+++ Musiksystem/src/data/DBCalls.java
@@ -1,5 +1,35 @@
 package data;
 
-public class DBCalls {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class DBCalls {
+	private static List getAllMusic(String whereClause) {
+		ArrayList array = new ArrayList();
+		try {
+			PreparedStatement stmt = JDBC.connection.prepareStatement("SELECT * FROM song, album, artist, conuctor WHERE keyword = ?");
+			stmt.setString(1, whereClause);
+			ResultSet rs = stmt.executeQuery();
+			
+//			System.out.println(rs);
+
+			while (rs.next()) {
+//				
+//				String songName = rs.getString("songName");
+//				String albumName = rs.getString("albumName");
+//				String artistName = rs.getString("artistName");
+//				String conductorName = rs.getString("conductorName");
+//				
+//				array.add(songName, albumName, artistName);			
+			}			
+		}
+		catch (SQLException e) {
+			System.out.println("Error executing SQL statement");
+			System.out.println(e.getMessage());
+		}
+		return array;
+	}
 }
