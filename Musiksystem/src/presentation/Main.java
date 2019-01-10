269437773c819e3682aee0931cb3173cb459244c
silke <silke@10.153.246.22>
2019-01-10 09:12:51 +0100

    ghg

--- Musiksystem/src/presentation/Main.java
+++ Musiksystem/src/presentation/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
-//		Song song = new Song(-1, 1, 2, 3, "21", Genre.ALTERNATIVE, "Blank", "Hej");
+//		Song song = new Song(6, 1, 2, 3, "21", Genre.ALTERNATIVE, "Blank", "Hej");
 //
 //		
-//		System.out.println(SongDBCalls.addSong(song));
+//		System.out.println(SongDBCalls.deleteSong(song));
 	}
 
 }
