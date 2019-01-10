d39f7962dac97c8d2d9e6c63d9be6d602ca954a2
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-10 11:01:59 +0100

    albumimpl

--- Musiksystem/src/logic/AlbumImpl.java
+++ Musiksystem/src/logic/AlbumImpl.java
@@ -1,7 +1,23 @@
 package logic;
 
+import data.AlbumDBCalls;
+import logic.domainClasses.Album;
+import logic.domainClasses.Song;
+
 public class AlbumImpl {
 
+	//AlbumDBCalls AlbumDBCalls = new AlbumDBCalls();
 	
+		public boolean createAlbum(Album album) {
+			return AlbumDBCalls.addAlbum(album);
+		}
+		
+		public boolean editAlbum(Album album) {
+			return AlbumDBCalls.updateAlbum(album);
+		}
+		
+		public boolean deleteAlbum(Album album) {
+			return AlbumDBCalls.deleteAlbum(album);
+		}
 	
 }
