cb18816c121ce6895e96bd8ede268db825af0fca
silke <silke@10.153.246.22>
2019-01-10 10:24:58 +0100

    fdgdf

---
+++ Musiksystem/src/presentation/TimeConverter.java
@@ -0,0 +1,14 @@
+package presentation;
+
+public class TimeConverter {
+	//TODO Mangler resterende genrer
+	
+	public int displayToSeconds() {
+		return -1;
+	}
+	
+	public String secondsToDisplay() {
+		return "3:31";
+	}
+	
+}
