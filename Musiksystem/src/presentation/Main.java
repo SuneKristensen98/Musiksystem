b317daf174a04c37509b66e5000c8273439ce787
jgaar <jgaar@10.153.238.40>
2019-01-10 09:49:27 +0100

    SongDBCalls Optimized

--- Musiksystem/src/presentation/Main.java
+++ Musiksystem/src/presentation/Main.java
@@ -8,10 +8,10 @@ public class Main {
 
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
-//		Song song = new Song(6, 1, 2, 3, "21", Genre.ALTERNATIVE, "Blank", "Hej");
+//		Song song = new Song(1, 1, 2, 3, "21 deluxe", Genre.ALTERNATIVE, 20, "blank", "Hej");
 //
 //		
-//		System.out.println(SongDBCalls.deleteSong(song));
+//		System.out.println(SongDBCalls.updateSong(song));
 	}
 
 }
