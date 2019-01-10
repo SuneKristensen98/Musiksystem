a2bba5142720c775354189e5972d29c45bdf5505
jgaar <jgaar@10.153.238.40>
2019-01-10 10:15:30 +0100

    Main Updated

--- Musiksystem/src/presentation/Main.java
+++ Musiksystem/src/presentation/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 //		Conductor conductor = new Conductor(1, "Silke");
-//	
+////
 //		System.out.println(DBCalls.addConductor(conductor));
 	}
 
