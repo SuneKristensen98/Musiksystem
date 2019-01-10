2bd9577dc33322e7a8dba843579bebe69fbeee4e
silke <silke@10.153.246.22>
2019-01-10 10:14:32 +0100

    try

--- Musiksystem/src/logic/domainClasses/Genre.java
+++ Musiksystem/src/logic/domainClasses/Genre.java
@@ -3,7 +3,7 @@ package logic.domainClasses;
 public enum Genre {
 	ALTERNATIVE("Alternativ"), BLUES("Blues"), CLASSICAL("Klassisk"), CLASSICROCK("Klassisk rock"), COUNTRY("Country"), 
 	ELECTRONICA("Elektronisk"), FOLK("Folkemusik"), HEAVYMETAL("Heavy metal"), HIPHOP("HipHop"), 
-	INDIEROCK(""), JAZZ("Jazz"), RAP("Rap"), RNB("RnB"), ROCK("Rock"), ROCKANDROLL("Rock n' roll"), 
+	INDIEROCK("Indie rock"), JAZZ("Jazz"), RAP("Rap"), RNB("RnB"), ROCK("Rock"), ROCKANDROLL("Rock n' roll"), 
 	POP("Pop"), PUNK("Punk"), SOUL("Soul"), SOUNDTRACKS("Soundtracks"), OTHER("Andet");
     
 	public String stringValue;
