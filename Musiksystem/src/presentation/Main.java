f5122a6768cf670fcd9134105477012f41e0bbdb
jgaar <jgaar@10.153.238.40>
2019-01-10 10:40:17 +0100

    Main Updated

--- Musiksystem/src/presentation/Main.java
+++ Musiksystem/src/presentation/Main.java
@@ -1,16 +1,35 @@
 package presentation;
 
-import data.SongDBCalls;
-import logic.domainClasses.Genre;
-import logic.domainClasses.Song;
+import data.DBCalls;
+import logic.domainClasses.Conductor;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+//		Artist artist = new Artist(1, "Johan");
+//		System.out.println(DBCalls.addArtist(artist));
+		
+//		Song song = new Conductor(1, "Silke");
+//		System.out.println(DBCalls.addConductor(conductor));
 		
 //		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
 //		System.out.println(SongDBCalls.addSong(song));
+		
+//		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
+//		System.out.println(SongDBCalls.deleteSong(song));
+		
+//		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
+//		System.out.println(SongDBCalls.updateSong(song));
+		
+//		Album album = new Album(5, "Sune's Tune", 2019, "CD", "Dope");
+//		System.out.println(AlbumDBCalls.addAlbum(album));
+		
+//		Album album = new Album(5, "Sune's Tune", 2019, "CD", "Dope");
+//		System.out.println(AlbumDBCalls.deleteAlbum(album));
+		
+//		Album album = new Album(5, "Sune's Tune", 2019, "CD", "Dope");
+//		System.out.println(AlbumDBCalls.updateAlbum(album));
 	}
 
 }
