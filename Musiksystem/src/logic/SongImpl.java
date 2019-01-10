a35c4e6aac1abe943aeb5660e3c13183bb3d2205
silke <silke@10.153.246.22>
2019-01-10 10:58:06 +0100

    gffg

--- Musiksystem/src/logic/SongImpl.java
+++ Musiksystem/src/logic/SongImpl.java
@@ -1,5 +1,21 @@
 package logic;
 
-public class SongImpl {
+import data.SongDBCalls;
+import logic.domainClasses.Song;
 
+public class SongImpl {
+	//SongDBCalls songDBCalls = new SongDBCalls();
+	
+	public boolean createSong(Song song) {
+		return SongDBCalls.addSong(song);
+	}
+	
+	public boolean editSong(Song song) {
+		return SongDBCalls.updateSong(song);
+	}
+	
+	public boolean deleteSong(Song song) {
+		return SongDBCalls.deleteSong(song);
+	}
+	
 }
--- Musiksystem/src/presentation/Main.java
+++ Musiksystem/src/presentation/Main.java
@@ -1,11 +1,17 @@
 package presentation;
 
 import data.DBCalls;
+import data.SongDBCalls;
+import logic.SongImpl;
 import logic.domainClasses.Conductor;
+import logic.domainClasses.Genre;
+import logic.domainClasses.Song;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		
 		
 //		Artist artist = new Artist(1, "Johan");
 //		System.out.println(DBCalls.addArtist(artist));
@@ -16,8 +22,10 @@ public class Main {
 //		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
 //		System.out.println(SongDBCalls.addSong(song));
 		
-//		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
+//		Song song = new Song(-1, 2, 2, 2, "Bravo", Genre.ROCK, 100, "Johan", "Dope");
 //		System.out.println(SongDBCalls.deleteSong(song));
+//		SongImpl songImpl = new SongImpl();
+//		System.out.println(songImpl.createSong(song));
 		
 //		Song song = new Song(5, 2, 2, 2, "Bravo", Genre.FOLK, 100, "Johan", "Dope");
 //		System.out.println(SongDBCalls.updateSong(song));
