1735e12ac82d49109e98f6ae8227047ae57e7116
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-11 09:20:47 +0100

    fx

--- Musiksystem/src/presentation/Editor.java
+++ Musiksystem/src/presentation/Editor.java
@@ -2,6 +2,7 @@ package presentation;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Editor {
@@ -13,6 +14,7 @@ public class Editor {
 		
 		BorderPane borderpane = new BorderPane();
 		Stage editor = new Stage();
+		editor.initModality(Modality.APPLICATION_MODAL);
 		editor.setTitle("editor");
 		Editor = new Scene(borderpane, 1600, 900);
 		editor.showAndWait();
--- Musiksystem/src/presentation/Table.java
+++ Musiksystem/src/presentation/Table.java
@@ -4,6 +4,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Table {
@@ -18,10 +19,11 @@ public class Table {
 		btnOpret.setPrefSize(100, 20);
 		btnOpret.setOnAction(e -> deleteaction());
 		BorderPane borderpane = new BorderPane();
-		borderpane.setCenter(label);
+		borderpane.setCenter(btnOpret);
 		tableStage.setTitle("table");
 		Table = new Scene(borderpane, 1600, 900);
 		tableStage.setScene(Table);
+		tableStage.setMaximized(true);
 		tableStage.show();
 		System.out.println("test1");
 	
