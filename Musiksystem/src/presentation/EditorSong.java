00676e9a856522e9e96df06d579a73a04c977253
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-15 13:49:21 +0100

    rødefelter

--- Musiksystem/src/presentation/EditorSong.java
+++ Musiksystem/src/presentation/EditorSong.java
@@ -15,189 +15,180 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import logic.BravoMusic;
-import logic.SongImpl;
 import logic.domainClasses.Genre;
-import logic.domainClasses.Song;
 
 public class EditorSong {
 
 	public VBox editorSong(BravoMusic bravoMusic) {
-
-		// Setup
-		SongImpl songImpl = new SongImpl();
-
+		
+		//Setup
 		VBox songBox = new VBox();
-		songBox.setPadding(new Insets(25, 25, 25, 25));
+		songBox.setPadding(new Insets(25, 25 ,25 ,25));
 		songBox.setAlignment(Pos.TOP_CENTER);
 		songBox.setPrefWidth(400);
-
+		
 //		String cssLayout = "-fx-border-color: grey;\n" +
 //                "-fx-border-insets: 25;\n" +
 //                "-fx-border-width: 3;\n" +
 //                "-fx-border-style: fill;\n";
 //		songBox.setStyle(cssLayout);
 		songBox.setBackground(Background.EMPTY);
-		String style = "-fx-background-color: rgba(100, 0, 255, 0.5);";
-		songBox.setStyle(style);
-
+		  String style = "-fx-background-color: rgba(100, 0, 255, 0.5);";
+		  songBox.setStyle(style);
+		
+		
 		HBox tidBox = new HBox(5);
 		tidBox.setAlignment(Pos.CENTER);
-
-		VBox btnBox = new VBox(25);
+		
+		VBox btnBox = new VBox(25);	
 		btnBox.setAlignment(Pos.BOTTOM_CENTER);
 		btnBox.setPrefHeight(1000);
-
-		// Label
+		
+		//Label
 		Label labelSongTitle = new Label("Sang");
 		labelSongTitle.setFont(Font.font("Helvetica", 20));
-
+		
 		Label labelGenre = new Label("Genre:");
 		labelGenre.setFont(Font.font("Helvetica", 16));
 		labelGenre.setPadding(new Insets(30, 0, 5, 0));
-
+		
 		Label labelKunstner = new Label("Kunstner:");
 		labelKunstner.setPadding(new Insets(25, 0, 5, 0));
 		labelKunstner.setFont(Font.font("Helvetica", 16));
-
+		
 		Label labelSangTitle = new Label("Sangtitel:");
 		labelSangTitle.setPadding(new Insets(25, 0, 0, 0));
 		labelSangTitle.setFont(Font.font("Helvetica", 16));
-
+		
 		Label labelTid = new Label("Tid:");
 		labelTid.setPadding(new Insets(25, 0, 5, 0));
 		labelTid.setFont(Font.font("Helvetica", 16));
-
+		
 		Label labelTidMin = new Label("Minutter:");
 		labelTidMin.setFont(Font.font("Helvetica", 14));
-
+		
 		Label labelTidSec = new Label("Sekunder:");
 		labelTidSec.setPadding(new Insets(0, 0, 0, 35));
 		labelTidSec.setFont(Font.font("Helvetica", 14));
-
+		
 		Label labelSangSkriver = new Label("Sangskriver:");
 		labelSangSkriver.setPadding(new Insets(25, 0, 5, 0));
 		labelSangSkriver.setFont(Font.font("Helvetica", 16));
-
+		
 		Label labelNote = new Label("Note:");
 		labelNote.setPadding(new Insets(25, 0, 5, 0));
 		labelNote.setFont(Font.font("Helvetica", 16));
-
+		
 		Label labelDirigent = new Label("Dirigent:");
 		labelDirigent.setPadding(new Insets(25, 0, 5, 0));
 		labelDirigent.setFont(Font.font("Helvetica", 16));
-
-		// Textfield
+			
+		//Textfield		
 		TextField tfKunstner = new TextField();
 		tfKunstner.setMaxWidth(1000);
 		tfKunstner.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfSangTitle = new TextField();
 		tfSangTitle.setMaxWidth(1000);
 		tfSangTitle.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfTidMin = new TextField();
 		tfTidMin.setMaxWidth(50);
 		tfTidMin.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfTidSec = new TextField();
 		tfTidSec.setMaxWidth(50);
 		tfTidSec.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfSangSkriver = new TextField();
 		tfSangSkriver.setMaxWidth(1000);
 		tfSangSkriver.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfNote = new TextField();
 		tfNote.setMaxWidth(1000);
 		tfNote.setFont(Font.font("Helvetica", 14));
-
+		
 		TextField tfDirigent = new TextField();
 		tfDirigent.setMaxWidth(1000);
 		tfDirigent.setFont(Font.font("Helvetica", 14));
-
-		// Buttons
+		
+		//Buttons
 		HBox songBoxBtn = new HBox(25);
 		songBoxBtn.setPadding(new Insets(50, 0, 0, 0));
 		songBoxBtn.setAlignment(Pos.CENTER);
-
+		
 		Button btnAdd = new Button("Tilføj");
 		btnAdd.setPrefSize(100, 20);
 		Button btnDelete = new Button("Slet");
 		btnDelete.setPrefSize(100, 20);
 		Button btnEdit = new Button("Redigere");
-		btnEdit.setPrefSize(100, 20);
-
-		// Set on Actions
-//		btnAdd.setOnAction(e -> addAction(bravoMusic)); {
-//		
-//		}
+		btnEdit.setPrefSize(100, 20);	
 		
-		btnDelete.setOnAction(e -> deleteAction(bravoMusic)); {
-
-		}
-		
-		btnEdit.setOnAction(e -> editAction(bravoMusic)); {
-
-		}
-		
-		// Genre combobox
+		//Genre combobox
 		ComboBox<Genre> genreCoB = new ComboBox<Genre>();
 		genreCoB.getItems().setAll(Arrays.asList(Genre.values()));
 		genreCoB.setPromptText("Genre");
 		genreCoB.setPrefHeight(32);
 		genreCoB.setMaxWidth(1000);
 		genreCoB.valueProperty().addListener(e -> {
+			//TODO fromString()
 //			genre = (Genre) genreCoB.getSelectionModel().getSelectedItem();
 //			table.updateTable(searchField.getText(), genre, lpChB.isSelected(), cdChB.isSelected());
 		});
-
+		
 		// force the field to be numeric only (Minutter)
 		tfTidMin.textProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (!newValue.matches("\\d*")) {
-					tfTidMin.setText(newValue.replaceAll("[^\\d]", ""));
-				}
-			}
+		    @Override
+		    public void changed(ObservableValue<? extends String> observable, String oldValue, 
+		        String newValue) {
+		        if (!newValue.matches("\\d*")) {
+		            tfTidMin.setText(newValue.replaceAll("[^\\d]", ""));
+		        }
+		    }
 		});
-
+		
 		// force the field to be numeric only (Sekunder)
-		tfTidSec.textProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				if (!newValue.matches("\\d*")) {
-					tfTidSec.setText(newValue.replaceAll("[^\\d]", ""));
+				tfTidSec.textProperty().addListener(new ChangeListener<String>() {
+				    @Override
+				    public void changed(ObservableValue<? extends String> observable, String oldValue, 
+				        String newValue) {
+				        if (!newValue.matches("\\d*")) {
+				            tfTidSec.setText(newValue.replaceAll("[^\\d]", ""));
+				        }
+				    }
+				});
+		
+		// Disable knapper		
+				btnAdd.setDisable(false);
+				btnDelete.setDisable(true);
+				btnEdit.setDisable(true);
+
+		
+		// Required fields
+				if (tfKunstner.getText().equals("")) {
+					tfKunstner.setPromptText("Skal udfyldes");
+					tfKunstner.setStyle("-fx-border-color: RED");
 				}
-			}
-		});
-
-		// Placement
-		songBox.getChildren().addAll(labelSongTitle, labelGenre, genreCoB, labelKunstner, tfKunstner, labelSangTitle,
-				tfSangTitle, labelTid, tidBox, labelSangSkriver, tfSangSkriver, labelNote, tfNote, labelDirigent,
-				tfDirigent, btnBox);
-
+				
+				if (tfSangTitle.getText().equals("")) {
+					tfSangTitle.setPromptText("Skal udfyldes");
+					tfSangTitle.setStyle("-fx-border-color: RED");
+				}
+						
+				
+				
+		//Placement
+		songBox.getChildren().addAll(labelSongTitle, labelGenre, genreCoB, labelKunstner, tfKunstner, labelSangTitle, tfSangTitle,
+				labelTid, tidBox, labelSangSkriver, tfSangSkriver, labelNote, tfNote, labelDirigent, tfDirigent, btnBox);
+		
 		btnBox.getChildren().addAll(songBoxBtn);
-
+		
 		tidBox.getChildren().addAll(labelTidMin, tfTidMin, labelTidSec, tfTidSec);
-
+		
 		songBoxBtn.getChildren().addAll(btnAdd, btnDelete, btnEdit);
-
-		// Return
+		
+		//Return
 		return songBox;
-
-	}
-	
-	private void addAction(BravoMusic bravoMusic, TextField tfSangTitle, Genre genreCoB, TextField tfTidMin, TextField tfSangSkriver, TextField tfNote) {
-//		Song song = new Song(-1, 1, 1, 1, tfSangTitle.getText(), genreCoB, tfTidMin.getText(), tfSangSkriver.getText(), tfNote.getText());
-//		bravoMusic.createSong(song);
-	}
-
-	private void deleteAction(BravoMusic bravoMusic) {
-//		bravoMusic.deleteSong(song);
-	}
-
-	private void editAction(BravoMusic bravoMusic) {
-//		bravoMusic.createSong(song);
 	}
 
 }
