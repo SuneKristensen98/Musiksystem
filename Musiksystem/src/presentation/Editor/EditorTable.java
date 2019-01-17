c11fc0f32909cd4acd01861368bbaef8f562406d
silke <silke@LAPTOP-PAGKBBGS>
2019-01-17 21:49:23 +0100

    Fiks af indryk

--- Musiksystem/src/presentation/Editor/EditorTable.java
+++ Musiksystem/src/presentation/Editor/EditorTable.java
@@ -1,56 +1,48 @@
 package presentation.Editor;
 
 import java.util.List;
-
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import logic.Impl;
-import logic.domainClasses.Song;
 import logic.domainClasses.TableViewInfo;
 import presentation.Factory;
 
 public class EditorTable {
 
+	Impl impl = new Impl();
+	private TableView<TableViewInfo> table = new TableView<>();
 
-		Impl impl = new Impl();
-		private TableView<TableViewInfo> table = new TableView<>();
-		
-		public EditorTable(VBox albumBot, int albumId, EditorSong editorSong) {
-			Factory factory = new Factory();
-			
-			TableColumn<TableViewInfo, String> songName = factory.columnFactoryString("songName", "Titel", 30);
-			TableColumn<TableViewInfo, String> artistName = factory.columnFactoryString("artistName", "Artist", 30);
-			List<TableViewInfo> allMusic = impl.searchMusic("", null, true, true, albumId);
+	public EditorTable(VBox albumBot, int albumId, EditorSong editorSong) {
+		Factory factory = new Factory();
 
-			table.getColumns().setAll(songName, artistName);
-			table.getItems().setAll(allMusic);
-			table.getSortOrder().setAll(songName);
-			table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-			
-			albumBot.getChildren().addAll(table);	
-			
-			table.getSelectionModel().selectedItemProperty().addListener(selection -> {
-				if (selection != null) {
-					editorSong.setTextFieldsFromTable(table.getSelectionModel().getSelectedItem());
-					editorSong.controlBtnDelete(false);
-					editorSong.controlBtnEdit(false);
-				}
-			});
-		}
-		
+		TableColumn<TableViewInfo, String> songName = factory.columnFactoryString("songName", "Titel", 30);
+		TableColumn<TableViewInfo, String> artistName = factory.columnFactoryString("artistName", "Artist", 30);
+		List<TableViewInfo> allMusic = impl.searchMusic("", null, true, true, albumId);
 
-		
-		public void updateTable(int albumId) {
-			List<TableViewInfo> musicFound = impl.searchMusic("", null, true, true, albumId);
-			table.getItems().setAll(musicFound);
-		}
-		
-		public TableViewInfo selectedRow() {
-			TableViewInfo selectedRow = table.getSelectionModel().getSelectedItem();
-			return selectedRow();
-		}
-		
+		table.getColumns().setAll(songName, artistName);
+		table.getItems().setAll(allMusic);
+		table.getSortOrder().setAll(songName);
+		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+		albumBot.getChildren().addAll(table);
+
+		table.getSelectionModel().selectedItemProperty().addListener(selection -> {
+			if (selection != null) {
+				editorSong.setTextFieldsFromTable(table.getSelectionModel().getSelectedItem());
+				editorSong.controlBtnDelete(false);
+				editorSong.controlBtnEdit(false);
+			}
+		});
 	}
-	
 
+	public void updateTable(int albumId) {
+		List<TableViewInfo> musicFound = impl.searchMusic("", null, true, true, albumId);
+		table.getItems().setAll(musicFound);
+	}
+
+	public TableViewInfo selectedRow() {
+		TableViewInfo selectedRow = table.getSelectionModel().getSelectedItem();
+		return selectedRow();
+	}
+
+}
