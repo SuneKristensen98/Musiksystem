555087b0cc85599a7cbd1ba4f8a25ae2bfc8aa9d
SuneKristensen98 <43539372+SuneKristensen98@users.noreply.github.com>
2019-01-25 09:13:35 +0100

    de gode animationer på opret og adm knapper

--- Musiksystem/src/presentation/MainSide/MainSideAlbumButtons.java
+++ Musiksystem/src/presentation/MainSide/MainSideAlbumButtons.java
@@ -1,8 +1,13 @@
 package presentation.MainSide;
 
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import logic.BravoMusic;
 import presentation.Factory;
 import presentation.Editor.Editor;
@@ -11,6 +16,7 @@ public class MainSideAlbumButtons {
 	private HBox returningHBox;
 	private Button btnAdm;
 	private Table table;
+	Glow glow = new Glow();
 
 	public HBox hBoxAlbumButtons(BravoMusic bravoMusic, Table tempTable) {
 		table = tempTable;
@@ -20,8 +26,35 @@ public class MainSideAlbumButtons {
 
 		Button btnCreate = factory.buttonFactory("Opret album", 88, false);
 		btnCreate.setStyle("-fx-background-color: MEDIUMPURPLE; -fx-font-weight: BOLD");
+		btnCreate.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				btnCreate.setEffect(glow);
+			}
+		});
+
+		btnCreate.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				btnCreate.setEffect(null);
+			}
+		});
+		
 		btnAdm = factory.buttonFactory("Adminstrer album", 119, true);
 		btnAdm.setStyle("-fx-background-color: MEDIUMPURPLE; -fx-font-weight: BOLD");
+		btnAdm.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				btnAdm.setEffect(glow);
+			}
+		});
+
+		btnAdm.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent e) {
+				btnAdm.setEffect(null);
+			}
+		});
 
 		btnCreate.setOnAction(e -> createAction(bravoMusic));
 		btnAdm.setOnAction(e -> updateAction(bravoMusic));
